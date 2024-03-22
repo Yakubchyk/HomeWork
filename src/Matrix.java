67db959457ce130b7b271c5f8cf7e769977d84b4
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-03-22 18:02:22 +0100

    Matrix mit Sterne

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,10 +1,10 @@
 public class Matrix {
     public static void main(String[] args) {
-        int[][] stars = new int[5][3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println(" ");
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
 
-        for (int i = 0; i <= 5; i++) {
-            for (int j = 0; j <= 3; i++) {
-                stars[i][j] = 1;
             }
 
         }
